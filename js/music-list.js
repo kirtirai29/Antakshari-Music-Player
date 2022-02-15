1a043f12a017065929e4ac8fa433a9a5762c3445
kirti <kirtirai2906@gmail.com>
2022-02-15 19:43:38 +0530

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+      name: "52 gaj ka daman",
+      artist: "Renuka Panwar",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Dil todega to pata laguga",
+      artist: "AP Dhillon",
+      img: "music-2",
+      src: "music-2"
+    },
+        {
+      name: "Dilruba",
+      artist: "Nirmal Roy & Shani Arshad",
+      img: "music-3",
+      src: "music-3"
+    },
+        {
+      name: "Thats all i really wanna do",
+      artist: "Shaan & Shreya Ghoshal",
+      img: "music-4",
+      src: "music-4"
+    },
+        {
+      name: "Ishqiya",
+      artist: "Azim Azhar",
+      img: "music-5",
+      src: "music-5"
+    }
+    ,
+        {
+      name: "Pyar bada karde gabru",
+      artist: "Romy",
+      img: "music-6",
+      src: "music-6"
+    }
+    ];
---
+++ music-list.js
@@ -0,0 +1,23 @@
+/*let allMusic = [
+    {
+      name: "King",
+      artist: "Jordan Schor",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "dil todega to pata laguga",
+      artist: "AP Dhillon",
+      img: "music-2",
+      src: "music-2"
+    }
+  
+    
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
